73ee41600a310e044a7b0c0c9180d5f76ef987dd
Toshi Singh <toshi.singh_cs24@gla.ac.in>
2026-02-12 15:15:45 +0530

    done

--- CLASSWORK/FIRST_SERVER/server.js
+++ CLASSWORK/FIRST_SERVER/server.js
@@ -1,13 +1,19 @@
+const user = {
+    username : "Toshi",
+    age : 19
+}
+
 const http =require("http");
 //console.log(http);
 const server = http.createServer((req, res) => {
-    const url = req.url;
+    const url =new URL (req.url, `http://${req.headers.host}`) ;
     const method = req.method;
-    if (method ==="GET" && url ==="/") {
-    res.writeHead(200, {"Content-Type":"text/plain" });
-    res.end("Welcome to the Home Page");
-}
+    const pathname = url.pathname;
 
+    if(method === "GET" && pathname === "/user"){
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(user));
+    }
 });
 server.listen(3000, ()=>{
     console.log("Server is running on port http://localhost:3000");
